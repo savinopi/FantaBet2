2fdc9d5a9a5100bf6a0e2b68c982352854dd5297
Savino Piemontese <piemonteses@int.master.lan>
2026-01-05 12:16:59 +0100

    feat: Ristrutturazione sezione Dati Lega con sottosezioni navigate - v3.2

--- commit.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "c:\Users\piemonteses\Desktop\MyDoc\Sito\FANTABet"
-git add -A
-git commit -m "feat: Ristrutturazione sezione Dati Lega con sottosezioni navigate e versioning - v3.2"
-git push origin main
-pause
